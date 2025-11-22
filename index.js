18900c3fd173c03604df53fe7b6828545c4cfc9e
Alekya Jogini <alekyajogini@Alekyas-MacBook-Air.local>
2025-11-22 01:02:28 -0500

    finalassigncode

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import express from "express";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
-import cors from "cors";
-import session from "express-session";
-import "dotenv/config";
-import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
+import ModuleRoutes from "./Kambaz/Modules/routes.js";
+import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
+import db from "./Kambaz/Database/index.js";
+import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
 
 const app = express();
+
 app.use(
   cors({
     credentials: true,
-    origin: process.env.CLIENT_URL || "http://localhost:3000", // Adjust the client URL as needed
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
   })
 );
-
-Lab5(app);
 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
@@ -27,15 +30,21 @@ if (process.env.SERVER_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL,
+    httpOnly: true, 
   };
 }
 
 app.use(session(sessionOptions));
 app.use(express.json());
 
-
-UserRoutes(app, db);
-
 const port = process.env.PORT || 4000;
+
+// Add routes AFTER session setup
+UserRoutes(app, db);
+CourseRoutes(app, db);
+ModuleRoutes(app, db);
+AssignmentRoutes(app, db);
+Hello(app);
+Lab5(app);
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
